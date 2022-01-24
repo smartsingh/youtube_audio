ef357137f7a73b27867a6eede52d10b2f6955bff
Iqbal Sandhu <smartsingh@gmail.com>
2022-01-23 19:01:09 -0800

    added more messaging for user experience

--- YouRip.py
+++ YouRip.py
@@ -5,14 +5,18 @@ import os
 import eyed3
 from urllib import request
 
+#creating video class using code that I developed in the audio scraper notebook
 class video:
     
     def __init__(self, url):
         
         self.yt = YouTube(url)
 
+        #storing track details for later
         self.track_title = self.yt.title
         self.uploader = self.yt.author
+
+        #grabbing image for album cover
 
         self.thumbnail = request.urlopen(self.yt.thumbnail_url)
         self.cover = self.thumbnail.read()
@@ -20,22 +24,30 @@ class video:
         print(f"Added {self.track_title} from {self.uploader}")
         
     def rip(self):
+        #gets stream with highest audio quality
         dl_stream = self.yt.streams.get_audio_only(subtype='mp4')
         
+        #downloads stream
         out_file = dl_stream.download(output_path='output')
         
         print(f"Ripping audio to {os.path.realpath(out_file)}")
 
+        #splits the extension for the output and gives us a name for the new file. Necessary for later
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp3'
         
+        #done with pytube, working with moviepy now
+
         conversion_file = AudioFileClip(out_file)
         conversion_file.write_audiofile(new_file)
         
+        #delete out file for space constraints
         os.remove(out_file)
         
+        #working with eyed3 now
         finished_file = eyed3.load(new_file)
 
+        #saves info from YouTube over to MP3
         finished_file.tag.title = self.track_title
         finished_file.tag.album = u"YouTube Downloads"
         finished_file.tag.artist = self.uploader
@@ -43,6 +55,7 @@ class video:
         finished_file.tag.save(version=eyed3.id3.ID3_V2_3)
         
         print(f"Finished downloading {self.track_title}")
+        os.system('afplay /System/Library/Sounds/Glass.aiff')
     
 
 class playlist:
@@ -50,6 +63,7 @@ class playlist:
     def __init__(self, url):
         self.plist = Playlist(url)
         
+        #making sure we're getting the right playlist
         self.p_title = self.plist.title
         self.owner = self.plist.owner
         
@@ -59,3 +73,17 @@ class playlist:
         for track in range(len(self.plist.video_urls)):
             p_track = video(self.plist.video_urls[track])
             p_track.rip()
+        
+        print(f"Finished downloading audio from Playlist {self.p_title}")
+        os.system('afplay /System/Library/Sounds/Glass.aiff')
+            
+dvid = input("Enter a Youtube link:")
+
+if dvid.find('watch') != -1:
+    vid_link = video(dvid)
+    vid_link.rip()
+elif dvid.find('playlist') != -1:
+    p_link = playlist(dvid)
+    p_link.rip()
+else:
+    print("This is not a valid link for download")
